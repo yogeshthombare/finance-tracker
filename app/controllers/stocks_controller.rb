de5f916186350755e596293e1551ece95fb9235d
Nikhil Bakshi <nikhilbakshi@alohatechnology.com>
2019-04-24 22:07:10 +0530

    Code refactor and Ajax request

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,20 @@
 class StocksController < ApplicationController
 
   def search 
-    @stock = Stock.new_from_lookup(params[:stock])
-    render "users/my_portfolio"
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
+      else 
+        flash[:danger] = "You have entered an incorrect symbol"
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:danger] = "No Search String Provided" 
+      redirect_to my_portfolio_path
+    end
   end
 
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,12 @@
 class Stock < ApplicationRecord
 
   def self.new_from_lookup(ticker_symbol)
-    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-    # price = strip_commas(looked_up_stock.latest_price)
-    Stock.new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+    begin
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      Stock.new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+    rescue Exception => e
+      return nil
+    end 
   end
 
   private
