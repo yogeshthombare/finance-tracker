70d47f8ee868b0e1c31c5c904f6ee8a779a33df9
Nikhil Bakshi <nikhilbakshi@alohatechnology.com>
2019-04-24 19:00:21 +0530

    Added Stock controller and showed stock on search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,13 @@
 class Stock < ApplicationRecord
+
+  def self.new_from_lookup(ticker_symbol)
+    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    # price = strip_commas(looked_up_stock.latest_price)
+    Stock.new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+  end
+
+  private
+    def self.strip_commas(number)
+      number.gsub(",", "")
+    end
 end
