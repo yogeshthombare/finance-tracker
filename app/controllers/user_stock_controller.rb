ae6a709cbaf81c29a0fc21e3101b0885c7b63e65
Yogesh Thombare <thombareyd@gmail.com>
2019-04-26 21:07:56 +0530

    Added stock to user and validations

---
+++ app/controllers/user_stock_controller.rb
@@ -0,0 +1,15 @@
+class UserStockController < ApplicationController
+
+  def create
+    stock =Stock.find_by_ticker(params[:stock_ticker])
+    if stock.blank?
+      stock = Stock.new_from_lookup(params[:stock_ticker])
+      stock.save
+    end
+    
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:success] = "Stock #{stock.name} is successfully added to your portfolio."
+    redirect_to my_portfolio_path
+  end
+  
+end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,8 @@
 class Stock < ApplicationRecord
 
+  has_many :user_stocks
+  has_many :users,  through: :user_stocks 
+
   def self.new_from_lookup(ticker_symbol)
     begin
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
@@ -7,6 +10,10 @@ class Stock < ApplicationRecord
     rescue Exception => e
       return nil
     end 
+  end
+
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 
   private
